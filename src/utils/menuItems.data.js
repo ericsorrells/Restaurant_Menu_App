75ccf57d6d536389871299ff19b2233f1417b601
Eric Sorrells <eric.sorrells@coxautoinc.com>
2021-09-20 22:05:06 -0400

    Added Menu Dropdown

--- src/utils/menuItems.data.js
+++ src/utils/menuItems.data.js
@@ -5,6 +5,7 @@ const menuItems = {
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
     price: 17.99,
     imageURL: "https://cdn11.bigcommerce.com/s-p6vajvx5jy/images/stencil/1280x1280/products/117/663/Meat_Lovers_2__84120.1556128972.jpg?c=2",
+    menu: "dinner"
   },
   2: {
     id: 2,
@@ -12,13 +13,15 @@ const menuItems = {
     description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
     price: 14.99,
     imageURL: "https://simply-delicious-food.com/wp-content/uploads/2020/06/Grilled-Pizza-Margherita-3-500x500.jpg",
+    menu: "dinner"
   },
   3: {
     id: 3,
     title: "BBQ Pizza",
     description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium",
     price: 12.99,
-    imageURL: "https://www.diabetesfoodhub.org/system/thumbs/system/images/recipes/Recid_76_BBQ_Chicken_Pizza_iStock-484653761_02262018_2959761669.jpg"
+    imageURL: "https://www.diabetesfoodhub.org/system/thumbs/system/images/recipes/Recid_76_BBQ_Chicken_Pizza_iStock-484653761_02262018_2959761669.jpg",
+    menu: "dinner",
   },
   4: {
     id: 4,
@@ -26,13 +29,47 @@ const menuItems = {
     description: "Et harum quidem rerum facilis est et expedita distinctio",
     price: 10.99,
     imageURL: "https://thecozyapron.com/wp-content/uploads/2021/01/calzone_thecozyapron_1.jpg",
+    menu: "dinner",
   },
   5: {
     id: 5,
     title: "Veggie Pizza",
     description: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam",
     price: 14.99,
-    imageURL: "https://www.twopeasandtheirpod.com/wp-content/uploads/2021/03/Veggie-Pizza-8-500x375.jpg"
+    imageURL: "https://www.twopeasandtheirpod.com/wp-content/uploads/2021/03/Veggie-Pizza-8-500x375.jpg",
+    menu: "dinner"
+  },
+  6: {
+    id: 6,
+    title: "Grilled Veggie Hoagie",
+    description: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam",
+    price: 8.99,
+    imageURL: "https://irepo.primecp.com/2015/08/233972/Overstuffed-Grilled-Veggie-Hoagie_MASTER_ID-1159482.jpg?v=1159482",
+    menu: "lunch"
+  },
+  7: {
+    id: 7,
+    title: "Panini",
+    description: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam",
+    price: 7.99,
+    imageURL: "https://dam.kraftheinzcompany.com/adaptivemedia/rendition/id_3566926e794027c9fd994bec11ad21db96b38868/ht_1000/wd_1500/name_./grilled-mexican-panini",
+    menu: "lunch"
+  },
+  8: {
+    id: 8,
+    title: "Cuban",
+    description: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam",
+    price: 7.99,
+    imageURL: "https://www.seriouseats.com/thmb/2OxF-UNLyWo2qMLSc6unSi7yH5o=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2016__06__20160623-cubano-roast-pork-sandwich-recipe-19-57695d21e77947538db375d1d30b4bdf.jpg",
+    menu: "lunch"
+  },
+  9: {
+    id: 9,
+    title: "Pineapple Ham Sandwich",
+    description: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam",
+    price: 9.99,
+    imageURL: "https://cdn3.tmbi.com/toh/GoogleImages/Hot-Pineapple-Ham-Sandwiches_EXPS_SDJJ17_201183_D02_16_6b.jpg",
+    menu: "lunch"
   }
  }
 
